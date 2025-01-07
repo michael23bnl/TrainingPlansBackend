a242ef73ec2ea841bb40bdd459268827f4d4bc11
michael23bnl <b2seversk70rus@gmail.com>
2025-01-07 22:40:01 +0700

    исправлены сущности микросервиса планов

---
+++ PlansMicroservice/Migrations/20250106061233_Migration3.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TrainingPlans.Migrations
+{
+    /// <inheritdoc />
+    public partial class Migration3 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Name",
+                table: "Plans",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "MuscleGroup",
+                table: "Exercises",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Name",
+                table: "Plans");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "MuscleGroup",
+                table: "Exercises",
+                type: "text",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+        }
+    }
+}
